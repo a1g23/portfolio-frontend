5a38645e4e03b3fdc81deac343b151a3786d8fc6
Alex Greenberg <acgreenberg2@gmail.com>
2024-02-03 13:00:02 -0500

    frontend completed

---
+++ src/loader.js
@@ -0,0 +1,15 @@
+// BASE URL OF OUR DEPLOYED API
+// BASE URL OF OUR DEPLOYED API
+const URL = "https://portfolio-ov6d.onrender.com"
+
+export const projectsLoader = async () => {
+    const response = await fetch(URL + "/projects")
+    const projects = await response.json()
+    return projects
+}
+
+export const aboutLoader = async () => {
+    const response = await fetch(URL + "/about")
+    const about = await response.json()
+    return about
+}
